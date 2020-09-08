0d498c92f6a2afdc277d23524f187c9340dd44e2
Alex Anderson <alexandersonone@gmail.com>
2020-09-08 16:36:33 -0700

    Fix syntax error

--- keylogger/logger_view.py
+++ keylogger/logger_view.py
@@ -24,15 +24,14 @@ def get_wpm_by_day():
     try:
         statstr = str(stats.get_stats())
     except Exception as e:
-        return "Error retrieving stats: \n" + str(e) + "\n"
-            "\nif this issue persists, please consider" +
+        return "Error retrieving stats: \n" + str(e) + \
+            "\nif this issue persists, please consider" + \
             "\ndeleting your key_log.txt file..."
     try:
         statstr = "\n".join(statstr.split("\n")[1:-1])
         return "WPM by day:\n" + statstr
     except:
         return "No data yet"
-
 
 def init_systray(env):
     tray = QSystemTrayIcon(QIcon(env['icon_file']))
